44313f5150ce161991fddf4096e490b35419e3a3
randy chng <randy.chng@fdmgroup.com>
2023-11-26 17:06:16 +0800

    Got correct path of chromedriver

--- app/services/hdbService.py
+++ app/services/hdbService.py
@@ -51,7 +51,7 @@ def start_driver(params:dict) -> webdriver.Chrome:
     # chrome_options.add_argument('--headless')  # Run in headless mode
     # driver = webdriver.Chrome(options=chrome_options)
     try:
-        service = webdriver.ChromeService(executable_path='/usr/bin/chromedriver')
+        service = webdriver.ChromeService(executable_path='/usr/local/bin/chromedriver')
         driver = webdriver.Chrome(service=service)
         driver.get(params["hdb_link"])
         assert params["hdb_title"] in driver.title
