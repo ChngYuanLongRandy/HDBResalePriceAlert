61687936f9467aa9b02024dd3327cef9aad36354
randy chng <randy.chng@fdmgroup.com>
2023-11-26 10:48:48 +0800

    Added chromdriver path for raspberrypi server

--- app/services/hdbService.py
+++ app/services/hdbService.py
@@ -50,7 +50,7 @@ def start_driver(params:dict) -> webdriver.Chrome:
     # chrome_options = Options()
     # chrome_options.add_argument('--headless')  # Run in headless mode
     # driver = webdriver.Chrome(options=chrome_options)
-    driver = webdriver.Chrome()
+    driver = webdriver.Chrome(executable_path='/usr/local/bin/chromedriver')
     driver.get(params["hdb_link"])
     assert params["hdb_title"] in driver.title
     return driver
