2941b2f107fd01bd86740ddd6eddf4b9ce037d76
chngyuanlongrandy <chngyuanlong@gmail.com>
2023-11-29 08:06:35 +0800

    Switch to original code

--- app/app.py
+++ app/app.py
@@ -74,25 +74,28 @@ def register():
         input_params["blk_from_val"] = data['blkNumberFrom']
         input_params['blk_to_val'] = data['blkNumberTo']
 
-        db= get_emails()
-        print("Printing all emails in db")
-        emails = []
-        for entry in db:
-            print(entry["email"])
-            emails.append(entry["email"])
+        # db= get_emails()
+        # print("Printing all emails in db")
+        # emails = []
+        # for entry in db:
+        #     print(entry["email"])
+        #     emails.append(entry["email"])
 
-        # Check if the email is already registered
-        if input_params["email"] in emails:
-            print("email exists, sending 400 response")
-            return jsonify({'error': 'Email is already registered'}), 400
+        # # Check if the email is already registered
+        # if input_params["email"] in emails:
+        #     print("email exists, sending 400 response")
+        #     return jsonify({'error': 'Email is already registered'}), 400
 
-        else:
-            # Do something with the email (e.g., store it, process it)
-            # For demonstration purposes, we are just adding it to a set
-            print("email does not exists, attempting to add ")
-            add_email(input_params)
-            print("email done adding, sending 200 response")
-            return jsonify({'message': 'Registration successful', 'data': input_params['email']}), 200
+        # else:
+        #     # Do something with the email (e.g., store it, process it)
+        #     # For demonstration purposes, we are just adding it to a set
+        #     print("email does not exists, attempting to add ")
+        #     add_email(input_params)
+        #     print("email done adding, sending 200 response")
+        #     return jsonify({'message': 'Registration successful', 'data': input_params['email']}), 200
+
+        add_email(input_params)
+        return jsonify({'message': 'Registration successful', 'data': input_params['email']}), 200
 
     except Exception as e:
         return jsonify({'error': str(e)}), 500
